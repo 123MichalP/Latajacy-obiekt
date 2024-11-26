f1fa815c76f0f499d49ce7f65e7e9f7761001053
123MichalP <michal.popieko@tm1.edu.pl>
2024-11-26 13:50:26 +0100

    Rock (png+script) and baloon changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const a=5; //Szybkosc poruszania sie balonu
+const a=8; //Szybkosc poruszania sie balonu
 var Balon = function(x, y) {
     this.x = x;
     this.y = y;
@@ -12,7 +12,8 @@ Balon.prototype.rysuj = function () {
     this.balonElement.css({
         position: "absolute",
         left: this.y + 'px',
-        top: this.y + 'px'  
+        top: this.y + 'px',
+        width: "200px",
     });
 
     $('body').append(this.balonElement);
@@ -21,7 +22,9 @@ Balon.prototype.rysuj = function () {
 var balon1 = new Balon(100, 100);
 balon1.rysuj();
 Balon.prototype.wPrawo = function(){
-    this.x+=a;
+    if (this.x + this.balonElement.width() < $(window).width()){
+        this.x+=a;
+    }
 
     this.balonElement.css({
         left:this.x,
@@ -29,7 +32,9 @@ Balon.prototype.wPrawo = function(){
     });
 };
 Balon.prototype.wLewo = function(){
+    if (this.x>-25){
     this.x-=a;
+    }
 
     this.balonElement.css({
         left:this.x,
@@ -37,7 +42,9 @@ Balon.prototype.wLewo = function(){
     });
 };
 Balon.prototype.wGore = function(){
-    this.y-=a;
+    if (this.y>-50){
+        this.y-=a;
+    }
 
     this.balonElement.css({
         left:this.x,
@@ -45,7 +52,9 @@ Balon.prototype.wGore = function(){
     });
 };
 Balon.prototype.wDol = function(){
-    this.y+=a;
+    if (this.y + this.balonElement.height() < $(window).height()) { 
+        this.y+=a;
+    }
 
     this.balonElement.css({
         left:this.x,
@@ -66,5 +75,55 @@ $(document).keydown(function(event) {
         balon1.wDol();  
     }
 });
+var Kamien = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = 5; // Prędkość w poziomie
+    this.vy = 5; // Prędkość w pionie
+    this.szerokosc = 120; 
+    this.wysokosc = 120; 
+};
 
+Kamien.prototype.rysuj = function () {
+    var kamienHtml = '<img src="rock.png" alt="kamien" class="kamien">';
+    this.kamienElement = $(kamienHtml);
+    
+    this.kamienElement.css({
+        position: "absolute",
+        left: this.x + 'px',
+        top: this.y + 'px',
+        width: this.szerokosc + 'px',
+        height: this.wysokosc + 'px'
+    });
 
+    $('body').append(this.kamienElement);
+};
+
+// Metoda poruszająca kamieniem i odbijająca go od ścian
+Kamien.prototype.ruch = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x <= 0 || this.x + this.szerokosc >= $(window).width()) {
+        this.vx = -this.vx; // Odbicie od ściany
+    }
+
+    if (this.y <= 0 || this.y + this.wysokosc >= $(window).height()) {
+        this.vy = -this.vy;
+    }
+
+    this.kamienElement.css({
+        left: this.x + 'px',
+        top: this.y + 'px'
+    });
+};
+
+var kamien1 = new Kamien(100, 100);
+kamien1.rysuj(); //zakomentowac, zeby usunac kamien
+
+function animacjaKamienia() {
+    kamien1.ruch();
+    requestAnimationFrame(animacjaKamienia)
+}
+
+animacjaKamienia();
