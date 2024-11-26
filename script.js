d70f92e4c4e929aa9400fa4a08fffad253344dfa
123MichalP <michal.popieko@tm1.edu.pl>
2024-11-26 13:14:06 +0100

    Baloon movement

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const a=5; //Szybkosc poruszania sie balonu
 var Balon = function(x, y) {
     this.x = x;
     this.y = y;
@@ -9,9 +10,9 @@ Balon.prototype.rysuj = function () {
     this.balonElement = $(balonHtml)
     
     this.balonElement.css({
-        posistion: "absolute",
-        right: this.x + 'px',
-        left: this.y + 'px'
+        position: "absolute",
+        left: this.y + 'px',
+        top: this.y + 'px'  
     });
 
     $('body').append(this.balonElement);
@@ -19,3 +20,51 @@ Balon.prototype.rysuj = function () {
 
 var balon1 = new Balon(100, 100);
 balon1.rysuj();
+Balon.prototype.wPrawo = function(){
+    this.x+=a;
+
+    this.balonElement.css({
+        left:this.x,
+        top:this.y
+    });
+};
+Balon.prototype.wLewo = function(){
+    this.x-=a;
+
+    this.balonElement.css({
+        left:this.x,
+        top:this.y
+    });
+};
+Balon.prototype.wGore = function(){
+    this.y-=a;
+
+    this.balonElement.css({
+        left:this.x,
+        top:this.y
+    });
+};
+Balon.prototype.wDol = function(){
+    this.y+=a;
+
+    this.balonElement.css({
+        left:this.x,
+        top:this.y
+    });
+};
+$(document).keydown(function(event) {
+    if (event.key === "ArrowRight") {  
+        balon1.wPrawo();  
+    }
+    if (event.key === "ArrowLeft") {  
+        balon1.wLewo();  
+    }
+    if (event.key === "ArrowUp") {  
+        balon1.wGore();  
+    }
+    if (event.key === "ArrowDown") {  
+        balon1.wDol();  
+    }
+});
+
+
