b3a98a0ed2f8a3364b39c3ff438519459550ada9
123MichalP <michal.popieko@tm1.edu.pl>
2024-12-03 13:11:19 +0100

    Ograniczenia ruchu i Kolizje

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const a = 4; //predkosc balona
+const a = 4; //prędkość balona
 var Balon = function(x, y) {
     this.x = x;
     this.y = y;
@@ -13,7 +13,7 @@ Balon.prototype.rysuj = function () {
 
     this.balonElement.css({
         position: "absolute",
-        left: this.y + 'px',
+        left: this.x + 'px',
         top: this.y + 'px',
         width: "200px",
     });
@@ -28,33 +28,50 @@ Balon.prototype.ruch = function() {
     this.x += this.vx;
     this.y += this.vy;
 
+    const balonWidth = this.balonElement.width();
+    const balonHeight = this.balonElement.height();
+    const windowWidth = $(window).width();
+    const windowHeight = $(window).height();
+
+    if (this.x < 0) this.x = 0;
+    if (this.x + balonWidth > windowWidth) this.x = windowWidth - balonWidth;
+    if (this.y < 0) this.y = 0;
+    if (this.y + balonHeight > windowHeight) this.y = windowHeight - balonHeight;
+
     this.balonElement.css({
-        left: this.x,
-        top: this.y
+        left: this.x + 'px',
+        top: this.y + 'px'
     });
 };
 
+
 $(document).keydown(function(event) {
-    if (event.key === "ArrowRight") {  
-        balon1.vx = a;  
-    }
-    if (event.key === "ArrowLeft") {  
-        balon1.vx = -a;  
-    }
-    if (event.key === "ArrowUp") {  
-        balon1.vy = -a;  
-    }
-    if (event.key === "ArrowDown") {  
-        balon1.vy = a;  
+    switch (event.key) {
+        case "ArrowRight":
+            balon1.vx = a;
+            break;
+        case "ArrowLeft":
+            balon1.vx = -a;
+            break;
+        case "ArrowUp":
+            balon1.vy = -a;
+            break;
+        case "ArrowDown":
+            balon1.vy = a;
+            break;
     }
 });
 
 $(document).keyup(function(event) {
-    if (event.key === "ArrowRight" || event.key === "ArrowLeft") {  
-        balon1.vx = 0;  
-    }
-    if (event.key === "ArrowUp" || event.key === "ArrowDown") {  
-        balon1.vy = 0;  
+    switch (event.key) {
+        case "ArrowRight":
+        case "ArrowLeft":
+            balon1.vx = 0;
+            break;
+        case "ArrowUp":
+        case "ArrowDown":
+            balon1.vy = 0;
+            break;
     }
 });
 
@@ -89,7 +106,6 @@ Knife.prototype.rysuj = function () {
     $('body').append(this.knifeElement);
 };
 
-// Metoda poruszająca nożem i odbijająca go od ścian
 Knife.prototype.ruch = function () {
     this.x += this.vx;
     this.y += this.vy;
@@ -101,35 +117,63 @@ Knife.prototype.ruch = function () {
     if (this.y <= 0 || this.y + this.wysokosc >= $(window).height()) {
         this.vy = -this.vy;
     }
-    if (this.vx > 0) {
-        this.kamienElement.css({
-            left: this.x + 'px',
-            top: this.y + 'px',
-            transform: 'rotate(' +  (Math.atan((this.vy)/(this.vx)) * 180 / Math.PI +45) + 'deg)'
-        });
-    } else {
-        this.kamienElement.css({
-            left: this.x + 'px',
-            top: this.y + 'px',
-            transform: 'rotate(' +  (Math.atan((this.vy)/(this.vx)) * 180 / Math.PI -135) + 'deg)'
-        });
-    }
 
+    // Obliczanie kąta rotacji noża
+    var angle = Math.atan2(this.vy, this.vx) * 180 / Math.PI;
 
-    
+    // Ustawienie rotacji noża
+    this.knifeElement.css({
+        left: this.x + 'px',
+        top: this.y + 'px',
+        transform: 'rotate(' + (angle+45) + 'deg)'
+    });
 };
 
-var knife1 = new Knife(100, 100);
-knife1.rysuj(); //zakomentowac, zeby usunac knife
+var knife1 = new Knife(500, 500);
+knife1.rysuj();
 
 function animacjaKnife() {
     knife1.ruch();
-    requestAnimationFrame(animacjaKnife)
+    requestAnimationFrame(animacjaKnife);
 }
 
 animacjaKnife();
 
 setInterval(function() {
-    knife1.vx *= 1.2;  
-    knife1.vy *= 1.2;  
-}, 10000); 
+    knife1.vx *= 1.15;  
+    knife1.vy *= 1.15;  
+}, 10000); //10s
+
+function sprawdzKolizje() {
+    if (!balon1 || !knife1) return; //czy istnieja
+
+    var balonLeft = balon1.x;
+    var balonRight = balon1.x + balon1.balonElement.width();
+    var balonTop = balon1.y;
+    var balonBottom = balon1.y + balon1.balonElement.height();
+
+    var knifeLeft = knife1.x;
+    var knifeRight = knife1.x + knife1.szerokosc;
+    var knifeTop = knife1.y;
+    var knifeBottom = knife1.y + knife1.wysokosc;
+
+    if (balonRight > knifeLeft && balonLeft < knifeRight &&
+        balonBottom > knifeTop && balonTop < knifeBottom) {
+        GameOver();
+    }
+}
+
+function animacja() {
+    balon1.ruch();
+    knife1.ruch();
+    sprawdzKolizje(); 
+    requestAnimationFrame(animacja);
+}
+animacja(); 
+
+
+function GameOver() {
+    console.log("Game Over");
+    cancelAnimationFrame(animacjaBalona);
+    cancelAnimationFrame(animacjaKnife);
+}
